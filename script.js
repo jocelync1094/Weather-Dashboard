05138dcc1e87f443d3e4f1325205a41e2f49e290
Jocelyn Chang <jocelync1094@gmail.com>
2019-10-26 19:40:31 -0700

    5-day forecast added

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ var currentWindSpd = $("<h6>");
 var currentUV = $("<h6>");
 var city = ""
 
+
+var fiveDayHeader = $("<h2>"+"5-Day Forecast:"+"</h2>");
+
 // click event of search
 
 $(".btn").on("click", function (e) {
@@ -14,6 +17,8 @@ $(".btn").on("click", function (e) {
     city = $(".city-input").val();
     console.log(city);
     currentWeatherData(city);
+    $("#current-weather").attr("style", "border: 1px solid lightgray; padding:10px")
+    forecastWeather(city);
 })
 
 
@@ -57,3 +62,32 @@ function currentWeatherData(city) {
 }
 
 
+// getting the 5 day forecast
+function forecastWeather(city){
+var forecastUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=f7cd9e34cec45ab16fa8240fea00b0b7&units=metric";
+var forecastObjectIndexEachDay =[0,7,15,23,31];
+$.ajax({
+    url:forecastUrl,
+    method:"GET"
+}).then(function(citySearched){
+    console.log(citySearched);
+    
+    for (var i=0;i<5;i++){
+        console.log("this is index", i);
+        var forecastDate = $("<h6>");
+        var forecastTemp =$("<h6>");
+        var forecastHumid=$("<h6>");
+        var dt = citySearched.list[forecastObjectIndexEachDay[i]].dt_txt;
+        dt = dt.substr(0,dt.indexOf(' '));
+        console.log(dt);
+        forecastDate.text(dt);
+        forecastTemp.text("Temp: " + citySearched.list[forecastObjectIndexEachDay[i]].main.temp +"C");
+        forecastHumid.text("Humidity: " + citySearched.list[forecastObjectIndexEachDay[i]].main.humidity +"%");
+       
+        $(".day"+i).append(forecastDate);
+        $(".day"+i).append(forecastTemp);
+        $(".day"+i).append(forecastHumid);
+    }
+    
+})
+}
